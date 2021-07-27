a5e23fa45829f4d342676935116bc4121652bba1
SW-GAGAN <srgaganreddy0@gmail.com>
2021-07-27 08:15:02 +0530

    Adding: Contacts to the Address Book

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,10 +1,16 @@
 package com.addressbook;
 
+/*
+@Description - To create a contacts in address book with first name, last name, address, city, state,
+* zip,mobile number.
+ */
 public class AddressBook {
-
     public static void main(String[] args) {
-        System.out.println("Welcome to address book");
+        Contacts contact = new Contacts("Gagan", "Reddy", " madivala",
+                "Benglore", "Karnataka", 560099,
+                91805666658L, "gaga@gmail.com");
+        System.out.println(contact.toString());
     }
-}
 
+}
 
